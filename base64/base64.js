96c4c2e72dd1ffb3dd1c8a6753a656a10aa987ac
Insomnia <915390118@qq.com>
2023-10-11 12:03:39 +0800

    Update Chinese Character Base64 Encode And Decode

--- base64/base64.js
+++ base64/base64.js
@@ -1,56 +1,79 @@
-function stringToBytes(str) {
-    let bytes = [];
-    let binaryCode = '';
-    for (let i = 0; i < str.length; i++) {
-        binaryCode += unicodeToByte(str.charCodeAt(i)).padStart(8, '0');
+class Base64 {
+    base64Collection = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split('');
+
+    // ==============================encode==============================
+    encode(str) {
+        let base64Code = '';
+        this.stringToBytes(str).forEach(element => {
+            base64Code += this.base64Collection[parseInt(element, 2)];
+        })
+        base64Code = this.supplyEqual(base64Code);
+        console.log('base64Code ', base64Code);
     }
-    for (let i = 0; i < binaryCode.length; i += 6) {
-        bytes.push(binaryCode.slice(i, i + 6).padEnd(6, '0'));
-    }
-    return bytes;
-}
 
-function unicodeToByte(unicode) {
-    return unicode.toString(2);
-}
-
-function encode(str) {
-    let base64Collection = 'ABCDEFGHIJKLMNOPQISTUVWXYZabcdefghijklmnopqistuvwxyz0123456789+/'.split('');
-    let base64Code = '';
-    stringToBytes(str).forEach(element => {
-        base64Code += base64Collection[parseInt(element, 2)];
-    })
-    base64Code = supplyEqual(base64Code);
-    console.log('base64Code ', base64Code);
-}
-
-function supplyEqual(base64Code) {
-    if (base64Code.length % 4 === 0) {
-        return base64Code;
-    } else {
-        base64Code += '=';
-        base64Code = supplyEqual(base64Code);
-    }
-    return base64Code;
-}
-
-// =========================================
-
-function decode(base64Code) {
-    let base64Collection = 'ABCDEFGHIJKLMNOPQISTUVWXYZabcdefghijklmnopqistuvwxyz0123456789+/'.split('');
-    let binaryCode = '';
-    let bytes = [];
-    let str = '';
-    for (let i = 0; i < base64Code.length; i++) {
-        if (base64Collection.indexOf(base64Code.charAt(i)) >= 0) {
-            binaryCode += base64Collection.indexOf(base64Code.charAt(i)).toString(2).padStart(6, '0');
+    stringToBytes(str) {
+        let bytes = [];
+        let binaryCode = '';
+        for (let i = 0; i < str.length; i++) {
+            const tempBinaryCode = this.toByte(str.charCodeAt(i));
+            if (tempBinaryCode.length <= 8) {
+                binaryCode += tempBinaryCode.padStart(8, '0');
+            } else {
+                binaryCode += this.toUFT8(tempBinaryCode.padStart(16, '0'));
+            }
         }
+        for (let i = 0; i < binaryCode.length; i += 6) {
+            bytes.push(binaryCode.slice(i, i + 6).padEnd(6, '0'));
+        }
+        return bytes;
     }
-    for (let i = 0; i < binaryCode.length; i += 8) {
-        bytes.push(binaryCode.slice(i, i + 8));
+
+    toByte(unicode) {
+        return unicode.toString(2);
     }
-    bytes.map(element => {
-        str += String.fromCharCode((parseInt(element, 2)));
-    })
-    console.log('string: ', str);
+
+    toUFT8(unicode) {
+        return '1110' + unicode.slice(0, 4) + '10' + unicode.slice(4, 10) + '10' + unicode.slice(10, 16);
+    }
+
+    supplyEqual(base64Code) {
+        if (base64Code.length % 4 === 0) {
+            return base64Code;
+        } else {
+            base64Code += '=';
+            base64Code = this.supplyEqual(base64Code);
+        }
+        return base64Code;
+    }
+
+    // ==============================decode==============================
+    decode(base64Code) {
+        let binaryCode = '';
+        let bytes = [];
+        let str = '';
+        const uft8Regex = /(1110[01]{4}10[01]{6}10[01]{6})/g;
+        for (let i = 0; i < base64Code.length; i++) {
+            if (this.base64Collection.indexOf(base64Code.charAt(i)) >= 0) {
+                binaryCode += this.base64Collection.indexOf(base64Code.charAt(i)).toString(2).padStart(6, '0');
+            }
+        }
+        binaryCode.split(uft8Regex).forEach(element => {
+            if (element.length === 24 && element.match(uft8Regex)) {
+                bytes.push(this.toUnicode(element));
+            } else {
+                for (let i = 0; i < element.length; i += 8) {
+                    bytes.push(element.slice(i, i + 8));
+                }
+            }
+        })
+        console.log(bytes)
+        bytes.map(element => {
+            str += String.fromCharCode((parseInt(element, 2)));
+        })
+        console.log('string: ', str);
+    }
+
+    toUnicode(utf8) {
+        return utf8.slice(4, 8) + utf8.slice(10, 16) + utf8.slice(18, 24);
+    }
 }
--- base64/base64.md
+++ base64/base64.md
@@ -49,6 +49,8 @@
 
 ## 3. 实现代码
 
+### 1. 实现ASCII码版本
+
 **JavaScript: **
 
 编码：
@@ -111,6 +113,94 @@ function decode(base64Code) {
         str += String.fromCharCode((parseInt(element, 2)));
     })
     console.log('string: ', str);
+}
+```
+
+### 2. 实现UFT-8版本
+
+> 汉字进行base64编码通常需要将汉字转化为UTF-8后再进行Base64的编码
+
+将先前代码进行更新并且结合ES6语法可得：
+
+```javascript
+class Base64 {
+    base64Collection = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split('');
+
+    // ==============================encode==============================
+    encode(str) {
+        let base64Code = '';
+        this.stringToBytes(str).forEach(element => {
+            base64Code += this.base64Collection[parseInt(element, 2)];
+        })
+        base64Code = this.supplyEqual(base64Code);
+        console.log('base64Code ', base64Code);
+    }
+
+    stringToBytes(str) {
+        let bytes = [];
+        let binaryCode = '';
+        for (let i = 0; i < str.length; i++) {
+            const tempBinaryCode = this.toByte(str.charCodeAt(i));
+            if (tempBinaryCode.length <= 8) {
+                binaryCode += tempBinaryCode.padStart(8, '0');
+            } else {
+                binaryCode += this.toUFT8(tempBinaryCode.padStart(16, '0'));
+            }
+        }
+        for (let i = 0; i < binaryCode.length; i += 6) {
+            bytes.push(binaryCode.slice(i, i + 6).padEnd(6, '0'));
+        }
+        return bytes;
+    }
+
+    toByte(unicode) {
+        return unicode.toString(2);
+    }
+
+    toUFT8(unicode) {
+        return '1110' + unicode.slice(0, 4) + '10' + unicode.slice(4, 10) + '10' + unicode.slice(10, 16);
+    }
+
+    supplyEqual(base64Code) {
+        if (base64Code.length % 4 === 0) {
+            return base64Code;
+        } else {
+            base64Code += '=';
+            base64Code = this.supplyEqual(base64Code);
+        }
+        return base64Code;
+    }
+
+    // ==============================decode==============================
+    decode(base64Code) {
+        let binaryCode = '';
+        let bytes = [];
+        let str = '';
+        const uft8Regex = /(1110[01]{4}10[01]{6}10[01]{6})/g;
+        for (let i = 0; i < base64Code.length; i++) {
+            if (this.base64Collection.indexOf(base64Code.charAt(i)) >= 0) {
+                binaryCode += this.base64Collection.indexOf(base64Code.charAt(i)).toString(2).padStart(6, '0');
+            }
+        }
+        binaryCode.split(uft8Regex).forEach(element => {
+            if (element.length === 24 && element.match(uft8Regex)) {
+                bytes.push(this.toUnicode(element));
+            } else {
+                for (let i = 0; i < element.length; i += 8) {
+                    bytes.push(element.slice(i, i + 8));
+                }
+            }
+        })
+        console.log(bytes)
+        bytes.map(element => {
+            str += String.fromCharCode((parseInt(element, 2)));
+        })
+        console.log('string: ', str);
+    }
+
+    toUnicode(utf8) {
+        return utf8.slice(4, 8) + utf8.slice(10, 16) + utf8.slice(18, 24);
+    }
 }
 ```
 
